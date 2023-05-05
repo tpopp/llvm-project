cc73d603e5bf6611bf1632a746e0d576df319b87
Tres Popp <tpopp@google.com>
2023-05-05 10:20:07 +0200

    [clang-tidy] Hacked check to update isa/dyn_cast/cast calls
    As described at https://mlir.llvm.org/deprecation/, function calls are
    preferred to methods on objects as they will work in more cases, so this
    provides suggestions everywhere the methods are used.
    
    This is just hacked together based on empirical results and with
    whatever I felt like doing that first worked, so the following happens:
    - I match on method calls for Attribute, Location, Value, Operation, and
      Op classes and their derived classes for cast, dyn_cast, isa, and
      dyn_cast_or_null.
    - Due to some details of SourceRange that I didn't want to figure out
      (returning the beginning of the last token which can be different than
      the last character), I avoid using it and instead manually split on "."
      or "->" (adding a new dereference "*") and then remove the "template"
      keyword when needed.
    
    Caveats to this flow:
    - In code that has defined a `cast` variable, cast will not work and one
      needs to manually add `llvm::` or rename the variable.
    - Inside of the defined methods of classes that support casting, `cast`
      and friends will refer to the method, so similarly, one needs to
      manually add `llvm::` prefixes or remove the methods at the same time.
    - Do not look at this code as an example of any good coding standard.
    - This won't catch generated code that is defined in strings or
      similarly

---
+++ clang-tools-extra/clang-tidy/misc/CastCheck.h
@@ -0,0 +1,35 @@
+//===--- CastCheck.h - clang-tidy -----------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISC_CHECKCHECK_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISC_CHECKCHECK_H
+
+#include "../ClangTidyCheck.h"
+#include "llvm/ADT/StringRef.h"
+#include <string>
+
+namespace clang::tidy::misc {
+
+/// Suggest replacements for `obj.cast<type>()` with `cast<type>(obj)` along
+/// with dyn_cast/dyn_cast_or_null/isa for all classes that support the
+/// functional calls in MLIR. This is not a perfect check and is only a
+/// temporary piece of code to ease migration.
+class CastCheck : public ClangTidyCheck {
+public:
+  CastCheck(StringRef Name, ClangTidyContext *Context);
+  bool isLanguageVersionSupported(const LangOptions &LangOpts) const override {
+    // XXX: I don't know what to put here, so always return true.
+    return true;
+  }
+  void registerMatchers(ast_matchers::MatchFinder *Finder) override;
+  void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+};
+
+} // namespace clang::tidy::misc
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISC_CHECKCHECK_H
